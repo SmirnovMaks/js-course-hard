9ca025c1608bfa9242e966781c114d424268cc70
Maksim Smirnov <perforator01@gmail.com>
2022-05-28 00:24:44 +0300

    исправил ошибки

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const num = 266219;
 
 let array = ("" + num).split("").map(Number);
-console.log(array);
-
 const multiplyNum = array.reduce((multiply, index) => multiply * index);
-
 console.log(multiplyNum);
 
 const cube = multiplyNum ** 3;
 console.log(cube);
+
 let twoSymbol = String(cube);
 let twoNumber = twoSymbol[0] + twoSymbol[1];
 let num2 = Number(twoNumber);
