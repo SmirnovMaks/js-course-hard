7c1ed1320b647eef11ed7eb4c5023cc1a88b4136
Maksim Smirnov <perforator01@gmail.com>
2022-06-01 02:58:05 +0300

    исправил ошибки

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const func = function (a) {
     if ((typeof a) == 'string') {
         let b = 30;
         a = a.replace(/^ +| +$/g, '');
-        return a.substr(0, b) + '...';
+        return a.substring(0, b) + '...';
     } else {
         return 'Аргумент функции не строка';
     }
